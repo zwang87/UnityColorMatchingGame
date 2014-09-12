5770225289d50e38c24c4b80346b467cd87a4c41
zwang87 <zw401@nyu.edu>
2014-09-11 20:15:11 -0400

    ColorMatchingGame
    Unity game, uses Oculus DK2, and Leap Motion control; Tap cubes to match
    the colors

---
+++ Assets/Scripts/CubeCollision.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeCollision : MonoBehaviour {
+    private Color[] colorMap = new Color[] { Color.red, Color.green, Color.blue, Color.yellow, Color.magenta, Color.cyan };
+    public Material alterMat;
+    public int count = 0;
+    private Material originMat;
+
+    private float lastTime = 0;
+    private float currentTime = 0;
+
+	// Use this for initialization
+	void Start () {
+        originMat = transform.renderer.material;
+	}
+
+    bool isColliding = false;
+    bool isExited = true;
+	// Update is called once per frame
+	void Update () {
+        isColliding = false;
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (isColliding || !isExited) return;
+        isColliding = true;
+        isExited = false;
+        currentTime = Time.realtimeSinceStartup;
+
+        float time = currentTime - lastTime;
+
+        Debug.Log(time);
+
+        if (time >= 0.3)
+        {
+            transform.renderer.material.color = colorMap[count % colorMap.Length];
+            count++;
+        }
+        lastTime = currentTime;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        isExited = true;
+    }
+}
---
+++ Assets/Scripts/CubeColor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CubeColor : MonoBehaviour {
+    
+    private const int num_cubesample = 3;
+    private const int num_cube = 3;
+    private const int num_color = 6;
+
+    private GameObject[] cubeSamples = new GameObject[num_cubesample];
+    private GameObject[] cubes = new GameObject[num_cube];
+
+    private Color[] colorMap = new Color[num_color]{Color.red, Color.green, Color.blue, Color.yellow, Color.magenta, Color.cyan};
+
+    private bool isMatched = false;
+
+    private GameObject particleSystem;
+	// Use this for initialization
+	void Start () {
+        cubeSamples[0] = GameObject.Find("CubeSample01");
+        cubeSamples[1] = GameObject.Find("CubeSample02");
+        cubeSamples[2] = GameObject.Find("CubeSample03");
+
+        cubes[0] = GameObject.Find("Cube01");
+        cubes[1] = GameObject.Find("Cube02");
+        cubes[2] = GameObject.Find("Cube03");
+        particleSystem = GameObject.Find("ParticleSystem");
+        RandomColor(cubeSamples, colorMap);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            RandomColor(cubeSamples, colorMap);
+        if (Input.GetKeyDown(KeyCode.Return))
+            RandomColor(cubes, colorMap);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            particleSystem.particleSystem.Play();
+        if (CheckCubeMatchStatus())
+        {
+            particleSystem.particleSystem.Play();
+            Debug.Log("GREAT!!!!");
+            RandomColor(cubeSamples, colorMap);
+            
+        }
+	}
+
+    bool CheckCubeMatchStatus()
+    { 
+        for(int i = 0; i < 3; i++)
+        { 
+            if(cubes[i].renderer.material.color != cubeSamples[i].renderer.material.color)
+                return false;
+        }
+        return true;
+    }
+
+    void RandomColor(GameObject[] cube, Color[] color)
+    {
+        int num_cube = cube.Length;
+        int num_color = color.Length;
+
+        for (int i = 0; i < num_cube; i++)
+        {
+            int colorIndex = Random.Range(0, num_color);
+            cube[i].renderer.material.color = colorMap[colorIndex];
+        }
+    }
+}
